70890bd6ecf687fee8534312a2888a91552e3a5e
Simonas Karuzas <simonas@not.cat>
2019-11-24 09:35:34 +0200

    feat: Graphql server in CLI

--- packages/daf-cli/src/cli.ts
+++ packages/daf-cli/src/cli.ts
@@ -4,6 +4,7 @@ import './did-resolver'
 import './credential'
 import './services'
 import './data-explorer'
+import './graphql'
 
 program.parse(process.argv)
 if (!process.argv.slice(2).length) {
--- packages/daf-cli/src/services.ts
+++ packages/daf-cli/src/services.ts
@@ -5,16 +5,18 @@ import program from 'commander'
 program
   .command('listen')
   .description('Receive new messages and listen for new ones')
-  .action(async (did) => {
-    await dataStore.initialize()
-
-    core.on(EventTypes.validatedMessage, (type, msg: Types.ValidatedMessage) => {
-      console.log('New message type:', msg.type)
-    })
-
-    await core.startServices()
-    await core.syncServices(
-      await dataStore.latestMessageTimestamps()
-    )
+  .action(async cmd => {
+    await listen()
   })
-  
+
+export const listen = async () => {
+  await dataStore.initialize()
+
+  core.on(EventTypes.validatedMessage, async (type, msg: Types.ValidatedMessage) => {
+    console.log('New message type:', msg.type)
+    await dataStore.saveMessage(msg)
+  })
+
+  await core.startServices()
+  await core.syncServices(await dataStore.latestMessageTimestamps())
+}
