b0b5b704bc8ceda994c9ca4b1e369b28192c1af1
Simonas Karuzas <simonas@not.cat>
2019-11-30 12:19:53 +0100

    fix: Debug info in daf-node-sqlite3

--- packages/daf-node-sqlite3/src/index.ts
+++ packages/daf-node-sqlite3/src/index.ts
@@ -1,6 +1,9 @@
 import sqlite3 from 'sqlite3'
 import { Types } from 'daf-data-store'
 
+import Debug from 'debug'
+const debug = Debug('daf-node-sqlite3')
+
 export class NodeSqlite3 implements Types.DbDriver {
   private db: any
 
@@ -9,6 +12,7 @@ export class NodeSqlite3 implements Types.DbDriver {
   }
 
   run(sql: string, params: any): Promise<boolean> {
+    debug('run', sql, params)
     return new Promise((resolve, reject) => {
       this.db.run(sql, params || [], function(err: any, result: any) {
         if (err) reject(err)
@@ -19,6 +23,7 @@ export class NodeSqlite3 implements Types.DbDriver {
   }
 
   rows(sql: string, params: any): Promise<any> {
+    debug('rows', sql, params)
     return new Promise((resolve, reject) => {
       this.db.all(sql, params || [], (err: any, result: any) => {
         if (err) reject(err)
