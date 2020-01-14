8061fbed8fc3626b165b8c03dee79901682f60a2
Simonas Karuzas <simonas@not.cat>
2020-01-14 13:46:14 +0200

    feat: GQL query to get latest service messages

--- packages/daf-core/src/graphql/graphql-core.ts
+++ packages/daf-core/src/graphql/graphql-core.ts
@@ -1,4 +1,6 @@
 import { Core } from '../core'
+import { LastMessageTimestampForInstance } from '../service/service-manager'
+
 import { Message } from '../message/message'
 
 interface Context {
@@ -21,13 +23,53 @@ const newMessage = async (
   )
 }
 
+const serviceMessagesSince = async (
+  _: any,
+  args: { ts: LastMessageTimestampForInstance[] },
+  ctx: Context,
+) => {
+  const res = await ctx.core.getMessagesSince(args.ts)
+  return res.map(msg => ({
+    ...msg,
+    data: JSON.stringify(msg.data),
+    raw: msg.raw,
+    metaData: msg.allMeta,
+  }))
+}
+
 export const resolvers = {
+  Query: {
+    serviceMessagesSince,
+  },
   Mutation: {
     newMessage,
   },
 }
 
 export const typeDefs = `
+  input LastMessageTimestampForInstance {
+    timestamp: Int!
+    did: String!
+    type: String!
+    id: String!
+  }
+
+  type ServiceMessage {
+    id: String!
+    threadId: String,
+    timestamp: Int,
+    sender: String,
+    receiver: String,
+    type: String,
+    raw: String,
+    data: String,
+    metaData: [MessageMetaData]
+  }
+
+  extend type Query {
+    serviceMessagesSince(ts: [LastMessageTimestampForInstance]!): [ServiceMessage]
+  }
+
   extend type Mutation {
     newMessage(raw: String!, sourceType: String!, sourceId: String): Message
   }
