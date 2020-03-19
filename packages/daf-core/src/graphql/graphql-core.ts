29bf414d907bc4ae8947e777034e35f9f15c3fa5
Simonas Karuzas <simonas@not.cat>
2020-03-19 10:59:09 +0200

    feat: More queries

--- packages/daf-core/src/graphql/graphql-core.ts
+++ packages/daf-core/src/graphql/graphql-core.ts
@@ -10,104 +10,160 @@ export interface Context {
   core: Core
 }
 
-const saveNewMessage = async (
-  _: any,
-  args: { raw: string; metaDataType?: string; metaDataValue?: string },
-  ctx: Context,
-) => {
-  return await ctx.core.saveNewMessage(args)
+export interface FindOptions {
+  take?: number, 
+  skip?: number,
 }
 
 const messages = async (
   _: any,
-  args:{ input?: { 
-    options?:{ take?: number, skip?: number },
-    from?: string[],
-    to?: string[],
-    type?: string,
-    threadId?: string,
-   }},
+  { input } : { 
+    input?: { 
+      options?: FindOptions,
+      from?: string[],
+      to?: string[],
+      type?: string[],
+      threadId?: string[],
+    }
+  },
 ) => {
   const options = { 
     where: {}
    }
 
-  if (args.input?.from) options.where['from'] = In(args.input.from)
-  if (args.input?.to) options.where['to'] = In(args.input.to)
-  if (args.input?.type) options.where['type'] = args.input.type
-  if (args.input?.threadId) options.where['threadId'] = args.input.threadId
-  if (args.input?.options?.skip) options['skip'] = args.input.options.skip
-  if (args.input?.options?.take) options['take'] = args.input.options.take
+  if (input?.from) options.where['from'] = In(input.from)
+  if (input?.to) options.where['to'] = In(input.to)
+  if (input?.type) options.where['type'] = In(input.type)
+  if (input?.threadId) options.where['threadId'] = In(input.threadId)
+  if (input?.options?.skip) options['skip'] = input.options.skip
+  if (input?.options?.take) options['take'] = input.options.take
 
-  return await Message.find(options)
+  return Message.find(options)
+}
+
+const presentations = async (
+  _: any,
+  { input } : { 
+    input?: { 
+      options?: FindOptions,
+      issuer?: string[],
+      audience?: string[],
+      type?: string[],
+      context?: string[],
+    }
+  },
+) => {
+  const options = { 
+    where: {}
+   }
+
+  if (input?.issuer) options.where['issuer'] = In(input.issuer)
+  if (input?.audience) options.where['audience'] = In(input.audience)
+  if (input?.type) options.where['type'] = In(input.type)
+  if (input?.context) options.where['context'] = In(input.context)
+  if (input?.options?.skip) options['skip'] = input.options.skip
+  if (input?.options?.take) options['take'] = input.options.take
+
+  return Presentation.find(options)
+}
+
+const credentials = async (
+  _: any,
+  { input } : { 
+    input?: { 
+      options?: FindOptions,
+      issuer?: string[],
+      subject?: string[],
+      type?: string[],
+      context?: string[],
+    }
+  },
+) => {
+  const options = { 
+    where: {}
+   }
+
+  if (input?.issuer) options.where['issuer'] = In(input.issuer)
+  if (input?.subject) options.where['audience'] = In(input.subject)
+  if (input?.type) options.where['type'] = In(input.type)
+  if (input?.context) options.where['context'] = In(input.context)
+  if (input?.options?.skip) options['skip'] = input.options.skip
+  if (input?.options?.take) options['take'] = input.options.take
+
+  return Credential.find(options)
 }
 
 const claims = async (
   _: any,
-  args:{ input?: { 
-    options?:{ take?: number, skip?: number },
-    issuer?: string,
-    subject?: string,
-    type?: string,
-    value?: string,
-   }},
+  { input }:{ 
+    input?: { 
+      options?: FindOptions,
+      issuer?: string[],
+      subject?: string[],
+      type?: string[],
+      value?: string[],
+    }
+  },
 ) => {
   const options = { 
     relations: ['credential'],
     where: {}
-   }
+  }
 
-  if (args.input?.issuer) options.where['issuer'] = args.input.issuer
-  if (args.input?.subject) options.where['subject'] = args.input.subject
-  if (args.input?.type) options.where['type'] = args.input.type
-  if (args.input?.value) options.where['value'] = args.input.value
-  if (args.input?.options?.skip) options['skip'] = args.input.options.skip
-  if (args.input?.options?.take) options['take'] = args.input.options.take
+  if (input?.issuer) options.where['issuer'] = In(input.issuer)
+  if (input?.subject) options.where['subject'] = In(input.subject)
+  if (input?.type) options.where['type'] = In(input.type)
+  if (input?.value) options.where['value'] = In(input.value)
+  if (input?.options?.skip) options['skip'] = input.options.skip
+  if (input?.options?.take) options['take'] = input.options.take
 
-  return await Claim.find(options)
+  return Claim.find(options)
 }
 
 export const resolvers = {
   Mutation: {
-    saveNewMessage,
+    saveNewMessage: async (_: any, args: { raw: string; metaDataType?: string; metaDataValue?: string }, ctx: Context) => ctx.core.saveNewMessage(args),
   },
 
   Query: {
-    messages,
-    message:    async (_: any, { id }) => Message.findOne(id),
     identity:   async (_: any, { did }) => Identity.findOne(did),
+    identities: async (_: any, { input }) => Identity.find({...input?.options}),
+    message:    async (_: any, { id }) => Message.findOne(id),
+    messages,
+    presentation: async (_: any, { hash }) => Presentation.findOne(hash),
+    presentations,
     credential: async (_: any, { hash }) => Credential.findOne(hash),
-    identities: async () => Identity.find(),
+    credentials,
+    claim: async (_: any, { hash }) => Claim.findOne(hash, { relations: ['credential'] }),
     claims,
   },
   
   Identity: {
-    sentMessages:          async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['sentMessages']})).sentMessages,
-    receivedMessages:      async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedMessages']})).receivedMessages,
-    issuedPresentations:   async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedPresentations']})).issuedPresentations,
-    receivedPresentations: async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedPresentations']})).receivedPresentations,
-    issuedCredentials:     async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedCredentials']})).issuedCredentials,
-    receivedCredentials:   async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedCredentials']})).receivedCredentials,
-    issuedClaims:          async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedClaims']})).issuedClaims,
-    receivedClaims:        async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedClaims']})).receivedClaims,
+    sentMessages:          async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['sentMessages'] })).sentMessages,
+    receivedMessages:      async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedMessages'] })).receivedMessages,
+    issuedPresentations:   async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedPresentations'] })).issuedPresentations,
+    receivedPresentations: async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedPresentations'] })).receivedPresentations,
+    issuedCredentials:     async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedCredentials'] })).issuedCredentials,
+    receivedCredentials:   async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedCredentials'] })).receivedCredentials,
+    issuedClaims:          async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['issuedClaims'] })).issuedClaims,
+    receivedClaims:        async (identity: Identity) => (await Identity.findOne(identity.did, { relations: ['receivedClaims'] })).receivedClaims,
   },
 
   Credential: {
-    claims:        async (credential: Credential) => Claim.find({ where: { credential: credential.hash}, relations: ['credential'] }),
-    messages:      async (credential: Credential) => (await Credential.findOne(credential.hash, { relations: ['messages']})).messages,
-    presentations: async (credential: Credential) => (await Credential.findOne(credential.hash, { relations: ['presentations']})).presentations,
+    claims:        async (credential: Credential) => (await Credential.findOne(credential.hash, { relations: ['claims'] })).claims,
+    messages:      async (credential: Credential) => (await Credential.findOne(credential.hash, { relations: ['messages'] })).messages,
+    presentations: async (credential: Credential) => (await Credential.findOne(credential.hash, { relations: ['presentations'] })).presentations,
   },
 
   Presentation: {
-    credentials: async (presentation: Presentation) => (await Presentation.findOne(presentation.hash, { relations: ['credentials']})).credentials,
-    messages:    async (presentation: Presentation) => (await Presentation.findOne(presentation.hash, { relations: ['messages']})).messages
+    credentials: async (presentation: Presentation) => (await Presentation.findOne(presentation.hash, { relations: ['credentials'] })).credentials,
+    messages:    async (presentation: Presentation) => (await Presentation.findOne(presentation.hash, { relations: ['messages'] })).messages
   },
 
   Message: {
-    presentations: async (message: Message) => (await Message.findOne(message.id, { relations: ['presentations']})).presentations,
-    credentials:   async (message: Message) => (await Message.findOne(message.id, { relations: ['credentials']})).credentials,
+    presentations: async (message: Message) => (await Message.findOne(message.id, { relations: ['presentations'] })).presentations,
+    credentials:   async (message: Message) => (await Message.findOne(message.id, { relations: ['credentials'] })).credentials,
   },
-
 
 }
 
@@ -118,32 +174,133 @@ export const typeDefs = `
     skip: Int
   }
 
+  input IdentitiesInput {
+    options: FindOptions
+  }
+
   input MessagesInput {
     from: [String]
     to: [String]
-    type: String
-    threadId: String
+    type: [String]
+    threadId: [String]
+    options: FindOptions
+  }
+
+  input PresentationsInput {
+    issuer: [String]
+    audience: [String]
+    type: [String]
+    context: [String]
+    options: FindOptions
+  }
+
+  input CredentialsInput {
+    issuer: [String]
+    subject: [String]
+    type: [String]
+    context: [String]
     options: FindOptions
   }
 
   input ClaimsInput {
-    issuer: ID
-    subject: ID
-    type: String
-    value: String
+    issuer: [ID]
+    subject: [ID]
+    type: [String]
+    value: [String]
     options: FindOptions
   }
 
   extend type Query {
     identity(did: ID!): Identity
-    identities: [Identity]
+    identities(input: IdentitiesInput): [Identity]
     message(id: ID!): Message
     messages(input: MessagesInput): [Message]
-    claims(input: ClaimsInput): [Claim]
+    presentation(hash: ID!): Presentation
+    presentations(input: PresentationsInput): [Presentation]
     credential(hash: ID!): Credential
+    credentials(input: CredentialsInput): [Credential]
+    claim(hash: ID!): Claim
+    claims(input: ClaimsInput): [Claim]
   }
 
   extend type Mutation {
     saveNewMessage(raw: String!, metaDataType: String, metaDataValue: String): Message
   }
+
+
+  extend type Identity {
+    sentMessages: [Message]
+    receivedMessages: [Message]
+    issuedPresentations: [Presentation]
+    receivedPresentations: [Presentation]
+    issuedCredentials: [Credential]
+    receivedCredentials: [Credential]
+    issuedClaims: [Claim]
+    receivedClaims: [Claim]
+  }
+
+  scalar Object
+  scalar Date
+  
+  type Message {
+    id: ID!
+    saveDate: Date!
+    updateDate: Date!
+    createdAt: Date
+    expiresAt: Date
+    threadId: String
+    type: String!
+    raw: String
+    data: Object
+    replyTo: [String]
+    replyUrl: [String]
+    from: Identity
+    to: Identity
+    metaData: [MetaData]
+    presentations: [Presentation]
+    credentials: [Credential]
+  }
+
+  type MetaData {
+    type: String!
+    value: String
+  }
+
+  type Presentation {
+    hash: ID!
+    raw: String!
+    issuer: Identity!
+    audience: Identity!
+    issuanceDate: Date!
+    expirationDate: Date
+    context: [String]
+    type: [String]
+    credentials: [Credential]
+    messages: [Message]
+  }
+  
+  type Credential {
+    hash: ID!
+    raw: String!
+    issuer: Identity!
+    subject: Identity!
+    issuanceDate: Date!
+    expirationDate: Date
+    context: [String]
+    type: [String]
+    credentialSubject: Object
+    claims: [Claim]
+    presentations: [Presentation]
+    messages: [Message]
+  }
+
+  type Claim {
+    hash: ID!
+    issuer: Identity!
+    subject: Identity!
+    credential: Credential!
+    type: String!
+    value: String!
+    isObj: Boolean
+  } 
 `
