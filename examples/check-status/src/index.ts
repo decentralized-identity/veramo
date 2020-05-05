4abb9e1b2a792868f7a7a1cf03f94091c8b0a398
Mircea Nistor <mirceanis@gmail.com>
2020-05-05 12:41:27 +0200

    chore: Add an example of revoked credential

--- examples/check-status/src/index.ts
+++ examples/check-status/src/index.ts
@@ -19,31 +19,17 @@ async function main() {
     data: {
       issuer: identity.did,
       '@context': ['https://www.w3.org/2018/credentials/v1'],
-      type: ['VerifiableCredential'],
+      type: ['VerifiableCredential', 'Revocable'],
       credentialSubject: {
         id: 'did:web:uport.me',
         you: 'Rock',
       },
+      credentialStatus: {
+        type: 'EthrStatusRegistry2019',
+        id: 'rinkeby:0x97fd27892cdcD035dAe1fe71235c636044B59348',
+      },
     },
   } as ActionSignW3cVc)
-
-  // // Sign verifiable credential
-  // const credential: Credential = await agent.handleAction({
-  //   type: W3cActionTypes.signCredentialJwt,
-  //   data: {
-  //     issuer: identity.did,
-  //     '@context': ['https://www.w3.org/2018/credentials/v1'],
-  //     type: ['VerifiableCredential', 'Revocable'],
-  //     credentialSubject: {
-  //       id: 'did:web:uport.me',
-  //       you: 'Rock',
-  //     },
-  //     // credentialStatus: {
-  //     //   type: 'EthrStatusRegistry2019',
-  //     //   id: 'rinkeby:0x97fd27892cdcD035dAe1fe71235c636044B59348'
-  //     // },
-  //   },
-  // } as ActionSignW3cVc)
 
   console.log('\n\n\n', credential)
 
@@ -51,36 +37,17 @@ async function main() {
     type: CredentialStatusActions.checkCredentialStatus,
     data: credential,
   })
+  console.log('\n\n\n Verification of a fresh credential:\n\n', verification)
 
-  console.log('\n\n\n', verification)
+  const revokedCredential =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1ODg2NjkwODMsInN1YiI6ImRpZDp3ZWI6dXBvcnQubWUiLCJub25jZSI6IjM4NzE4Njc0NTMiLCJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiQXdlc29tZW5lc3NDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7Iml0IjoicmVhbGx5IHdoaXBzIHRoZSBsbGFtbWEncyBhc3MhIn19LCJjcmVkZW50aWFsU3RhdHVzIjp7InR5cGUiOiJFdGhyU3RhdHVzUmVnaXN0cnkyMDE5IiwiaWQiOiJyaW5rZWJ5OjB4OTdmZDI3ODkyY2RjRDAzNWRBZTFmZTcxMjM1YzYzNjA0NEI1OTM0OCJ9LCJpc3MiOiJkaWQ6ZXRocjoweDU0ZDU5ZTNmZmQ3NjkxN2Y2MmRiNzAyYWMzNTRiMTdmMzg0Mjk1NWUifQ.39eegtmP3oyudaWXTXVgA7wIY610Gz0Hio4ivuFxhHwFnUdXkqiNHcf32Gwnoo05IiKeIjTl_3vYxdttm38pqA'
 
-  // const status = new Status({
-  //   ...new EthrStatusRegistry({ infuraProjectId: '6b734e0b04454df8a6ce234023c04f26' }).asStatusMethod,
-  // })
+  const verification2: Verified = await agent.handleAction({
+    type: CredentialStatusActions.checkCredentialStatus,
+    data: revokedCredential,
+  })
 
-  // const didDoc = await agent.didResolver.resolve(credential.issuer.did)
-
-  // const result = await status.checkStatus(credential.raw, didDoc)
-
-  // console.log('revocation-status', result)
-
-  //   Send verifiable credential using DIDComm
-  //   const message = await agent.handleAction({
-  //     type: ActionTypes.sendMessageDIDCommAlpha1,
-  //     data: {
-  //       from: identity.did,
-  //       to: 'did:web:uport.me',
-  //       type: 'jwt',
-  //       body: credential.raw,
-  //     },
-  //   } as ActionSendDIDComm)
-  //   console.log({ message })
+  console.log('\n\n\n Verification of a revoked credential:\n\n', verification2)
 }
-
-// // This is triggered when DAF successfully saves a new message
-// // which can arrive from external services, or by sending it using `action.sendJwt`
-// agent.on(EventTypes.savedMessage, async (message: Message) => {
-//   console.log('\n\nSuccessfully sent message:', message)
-// })
 
 main().catch(console.log)
