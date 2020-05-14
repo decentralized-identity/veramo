eeb77a68ab9bc2c9d41caa8bbbf526565626ffad
Roderik van der Veer <roderik@settlemint.com>
2020-05-14 07:59:29 +0200

    fix: Re-exporting a type requires 'export type'

--- packages/daf-core/src/index.ts
+++ packages/daf-core/src/index.ts
@@ -1,22 +1,23 @@
-export { Agent, EventTypes, Resolver } from './agent'
+export { Agent, EventTypes } from './agent'
+export type { Resolver } from './agent'
 export { AbstractActionHandler } from './action/action-handler'
 export { IdentityManager } from './identity/identity-manager'
 export { AbstractIdentity } from './identity/abstract-identity'
 export { AbstractIdentityController } from './identity/abstract-identity-controller'
-export { AbstractIdentityProvider, IdentityProviderDerived } from './identity/abstract-identity-provider'
-export {
-  AbstractKeyManagementSystem,
-  AbstractKey,
-  SerializedKey,
-} from './identity/abstract-key-management-system'
-export { AbstractIdentityStore, SerializedIdentity } from './identity/abstract-identity-store'
+export { AbstractIdentityProvider } from './identity/abstract-identity-provider'
+export type { IdentityProviderDerived } from './identity/abstract-identity-provider'
+export { AbstractKeyManagementSystem, AbstractKey } from './identity/abstract-key-management-system'
+export type { SerializedKey } from './identity/abstract-key-management-system'
+export { AbstractIdentityStore } from './identity/abstract-identity-store'
+export type { SerializedIdentity } from './identity/abstract-identity-store'
 export { AbstractKeyStore } from './identity/abstract-key-store'
 export { AbstractSecretBox } from './identity/abstract-secret-box'
 export { AbstractMessageHandler } from './message/abstract-message-handler'
-export { ServiceManager, LastMessageTimestampForInstance, ServiceEventTypes } from './service/service-manager'
+export { ServiceManager, ServiceEventTypes } from './service/service-manager'
+export type { LastMessageTimestampForInstance } from './service/service-manager'
 export { AbstractServiceController } from './service/abstract-service-controller'
 export { Gql } from './graphql/index'
-export { Action } from './types'
+export type { Action } from './types'
 export { IdentityStore } from './identity/identity-store'
 export { KeyStore } from './identity/key-store'
 
@@ -29,6 +30,7 @@ import { Message, MetaData } from './entities/message'
 
 export const Entities = [Key, Identity, Message, Claim, Credential, Presentation]
 
-export { KeyType, Key, Identity, Message, Claim, Credential, Presentation, MetaData }
+export { Key, Identity, Message, Claim, Credential, Presentation }
+export type { KeyType, MetaData }
 
 export { migrations } from './migrations'
