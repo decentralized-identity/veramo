758f066517b6c9d6eacd9e039bf5ddd6a3f55efc
Roderik van der Veer <roderik@settlemint.com>
2020-05-18 15:23:11 +0200

    feat: Expose the CredentialInput type from action-handler

--- packages/daf-w3c/src/index.ts
+++ packages/daf-w3c/src/index.ts
@@ -1,4 +1,5 @@
 export { W3cMessageHandler, MessageTypes } from './message-handler'
 export { W3cActionHandler, ActionTypes, ActionSignW3cVp, ActionSignW3cVc } from './action-handler'
+export type {CredentialInput} from './action-handler'
 import W3cGql from './graphql'
 export { W3cGql }
