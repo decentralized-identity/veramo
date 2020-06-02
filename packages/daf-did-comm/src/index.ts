d578f53064e200899039f3b02668811117f4607e
Simonas Karuzas <simonas@not.cat>
2020-06-02 12:50:40 +0300

    refactor: DIDComm

--- packages/daf-did-comm/src/index.ts
+++ packages/daf-did-comm/src/index.ts
@@ -1,8 +1,7 @@
 import { IAgentPlugin, TMethodMap } from 'daf-core'
-import { IAgentSendMessageDIDCommAlpha1, sendMessageDIDCommAlpha1 } from './action-handler'
+import { sendMessageDIDCommAlpha1 } from './action-handler'
+export { IAgentSendMessageDIDCommAlpha1 } from './action-handler'
 export { DIDCommMessageHandler } from './message-handler'
-
-export type IAgentDIDComm = IAgentSendMessageDIDCommAlpha1
 
 export class DIDComm implements IAgentPlugin {
   readonly methods: TMethodMap = {
