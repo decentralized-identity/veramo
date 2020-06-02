27fd623cc5d536b1b73803988a06f21dd9c1844c
Simonas Karuzas <simonas@not.cat>
2020-06-02 11:08:57 +0300

    refactor: Resolvers as IAgentPlugin

--- packages/daf-resolver-universal/src/resolver.ts
+++ packages/daf-resolver-universal/src/resolver.ts
@@ -1,25 +1,42 @@
 import 'cross-fetch/polyfill'
+import { IContext, TMethodMap, IAgent, IAgentPlugin } from 'daf-core'
+import { DIDDocument } from 'did-resolver'
+export { DIDDocument }
 import Debug from 'debug'
-
 const debug = Debug('daf:resolver-universal')
 
 interface Options {
   url: string
 }
 
-export class DafUniversalResolver {
+interface IArgs {
+  did: string
+}
+
+export interface IAgentResolve extends IAgent {
+  resolve?: (args: IArgs) => Promise<DIDDocument>
+}
+
+export class DafUniversalResolver implements IAgentPlugin {
+  public methods: TMethodMap
   private url: string
 
   constructor(options: Options) {
     if (!options.url) throw Error('[daf-resolver-universal] url required')
     debug(options.url)
     this.url = options.url
+
+    this.resolve = this.resolve.bind(this)
+
+    this.methods = {
+      resolve: this.resolve,
+    }
   }
 
-  async resolve(did: string) {
-    debug('Resolving %s', did)
+  async resolve(args: IArgs, context: IContext): Promise<DIDDocument> {
+    debug('Resolving %s', args.did)
     try {
-      const result = await fetch(this.url + did)
+      const result = await fetch(this.url + args.did)
       const ddo = await result.json()
       return ddo.didDocument
     } catch (e) {
