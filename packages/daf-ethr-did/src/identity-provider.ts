fe332fa6be9f3824512c24f979fac90939aa664c
Simonas Karuzas <simonas@not.cat>
2020-06-11 17:50:34 +0300

    refactor: Ethr identity provider (WIP)

--- packages/daf-ethr-did/src/identity-provider.ts
+++ packages/daf-ethr-did/src/identity-provider.ts
@@ -1,16 +1,16 @@
-import {
-  AbstractIdentityProvider,
-  AbstractIdentity,
-  AbstractIdentityStore,
-  AbstractKeyManagementSystem,
-  SerializedIdentity,
-} from 'daf-core'
+import { IIdentity, IKey, IService, IAgentBase, IAgentKeyManager, AbstractIdentityProvider } from 'daf-core'
 import { Identity } from './identity'
 import { IdentityController } from './identity-controller'
 import { keccak_256 } from 'js-sha3'
-const SignerProvider = require('ethjs-provider-signer')
 import Debug from 'debug'
+const EthrDID = require('ethr-did')
+const SignerProvider = require('ethjs-provider-signer')
 const debug = Debug('daf:ethr-did:identity-provider')
+
+interface IContext {
+  agent: IAgentBase & IAgentKeyManager
+}
+
 
 export function toEthereumAddress(hexPublicKey: string): string {
   return `0x${Buffer.from(keccak_256.arrayBuffer(Buffer.from(hexPublicKey.slice(2), 'hex')))
@@ -19,20 +19,14 @@ export function toEthereumAddress(hexPublicKey: string): string {
 }
 
 export class IdentityProvider extends AbstractIdentityProvider {
-  public type = 'ethr-did'
-  public description = 'identities'
   private network: string
   private web3Provider?: any
   private rpcUrl?: string
-  private kms: AbstractKeyManagementSystem
-  private identityStore: AbstractIdentityStore
   private gas?: number
   private ttl?: number
   private registry?: string
 
   constructor(options: {
-    kms: AbstractKeyManagementSystem
-    identityStore: AbstractIdentityStore
     network: string
     rpcUrl?: string
     web3Provider?: object
@@ -41,21 +35,79 @@ export class IdentityProvider extends AbstractIdentityProvider {
     registry?: string
   }) {
     super()
-    this.kms = options.kms
-    this.identityStore = options.identityStore
     this.network = options.network
     this.rpcUrl = options.rpcUrl
     this.web3Provider = options.web3Provider
-    this.type = options.network + '-' + this.type
-    this.description = 'did:ethr ' + options.network + ' ' + this.description
     this.ttl = options.ttl
     this.gas = options.gas
     this.registry = options.registry
   }
 
-  private async identityFromSerialized(serializedIdentity: SerializedIdentity): Promise<AbstractIdentity> {
-    const key = await this.kms.getKey(serializedIdentity.controllerKeyId)
+  // private async identityFromSerialized(serializedIdentity: SerializedIdentity): Promise<AbstractIdentity> {
+  //   const key = await this.kms.getKey(serializedIdentity.controllerKeyId)
 
+  //   if (!this.web3Provider && !this.rpcUrl) throw Error('Web3Provider or rpcUrl required')
+
+  //   const web3Provider =
+  //     this.web3Provider ||
+  //     new SignerProvider(this.rpcUrl, {
+  //       signTransaction: key.signEthTransaction.bind(key),
+  //     })
+
+  //   const identityController = new IdentityController({
+  //     did: serializedIdentity.did,
+  //     web3Provider,
+  //     kms: this.kms,
+  //     identityStore: this.identityStore,
+  //     address: toEthereumAddress(key.serialized.publicKeyHex),
+  //     gas: this.gas,
+  //     ttl: this.ttl,
+  //     registry: this.registry,
+  //   })
+
+  //   return new Identity({
+  //     serializedIdentity,
+  //     kms: this.kms,
+  //     identityProviderType: this.type,
+  //     identityController,
+  //   })
+  // }
+
+  // async getIdentities() {
+  //   const dids = await this.identityStore.listDids()
+  //   const result = []
+  //   for (const did of dids) {
+  //     const serializedIdentity = await this.identityStore.get(did)
+  //     result.push(await this.identityFromSerialized(serializedIdentity))
+  //   }
+  //   return result
+  // }
+
+  async createIdentity(
+    { kms, options }: { kms: string; options?: any },
+    context: IContext,
+  ): Promise<Omit<IIdentity, 'provider'>> {
+
+    const key = await context.agent.keyManagerCreateKey({ kms, type: 'Secp256k1' })
+    const address = toEthereumAddress(key.publicKeyHex)
+    const identity: Omit<IIdentity, 'provider'> = {
+      did: 'did:ethr:' + (this.network !== 'mainnet' ? this.network + ':' : '') + address,
+      controllerKeyId: key.kid,
+      keys: [key],
+      services: []
+    }
+    debug('Created', identity.did)
+    return identity
+  }
+
+  async deleteIdentity(identity: IIdentity, context: IContext): Promise<boolean> {
+    for (const { kid } of identity.keys) {
+      await context.agent.keyManagerDeleteKey({ kid })
+    }
+    return true
+  }
+
+  private getWeb3Provider(key: IKey) {
     if (!this.web3Provider && !this.rpcUrl) throw Error('Web3Provider or rpcUrl required')
 
     const web3Provider =
@@ -63,73 +115,54 @@ export class IdentityProvider extends AbstractIdentityProvider {
       new SignerProvider(this.rpcUrl, {
         signTransaction: key.signEthTransaction.bind(key),
       })
+  }
 
-    const identityController = new IdentityController({
-      did: serializedIdentity.did,
-      web3Provider,
-      kms: this.kms,
-      identityStore: this.identityStore,
-      address: toEthereumAddress(key.serialized.publicKeyHex),
-      gas: this.gas,
-      ttl: this.ttl,
+  async addKey({ did, key, options }: { did: string; key: IKey; options?: any }): Promise<any> {
+
+    const ethrDid = new EthrDID({
+      address: did.split(':').pop,
+      provider: this.web3Provider,
       registry: this.registry,
     })
 
-    return new Identity({
-      serializedIdentity,
-      kms: this.kms,
-      identityProviderType: this.type,
-      identityController,
+    const usg = 'veriKey'
+    const attribute = 'did/pub/' + key.type + '/' + usg + '/hex'
+    const value = '0x' + key.publicKeyHex
+    const ttl = options?.ttl || this.ttl 
+    const gas = options?.gas || this.gas 
+    
+    debug('ethrDid.setAttribute', { attribute, value, ttl, gas })
+
+    const txHash = await ethrDid.setAttribute(attribute, value, ttl, gas)
+    debug({ txHash })
+    return txHash
+  }
+
+  async addService({ did, service, options }: { did: string; service: IService; options?: any }): Promise<any> {
+    const ethrDid = new EthrDID({
+      address: did.split(':').pop,
+      provider: this.web3Provider,
+      registry: this.registry,
     })
+
+    const attribute = 'did/svc/' + service.type
+    const value = service.serviceEndpoint
+    const ttl = options?.ttl || this.ttl 
+    const gas = options?.gas || this.gas 
+
+    debug('ethrDid.setAttribute', { attribute, value, ttl, gas })
+    
+    const txHash = await ethrDid.setAttribute(attribute, value, ttl, gas)
+    debug({ txHash })
+    return txHash
   }
 
-  async getIdentities() {
-    const dids = await this.identityStore.listDids()
-    const result = []
-    for (const did of dids) {
-      const serializedIdentity = await this.identityStore.get(did)
-      result.push(await this.identityFromSerialized(serializedIdentity))
-    }
-    return result
+  async removeKey(args: { did: string; kid: string; options?: any }): Promise<any> {
+    throw Error('Not implemented')
   }
 
-  async createIdentity() {
-    const key = await this.kms.createKey('Secp256k1')
-    const address = toEthereumAddress(key.serialized.publicKeyHex)
-    const serializedIdentity: SerializedIdentity = {
-      did: 'did:ethr:' + (this.network !== 'mainnet' ? this.network + ':' : '') + address,
-      controllerKeyId: key.serialized.kid,
-      keys: [key.serialized],
-    }
-    await this.identityStore.set(serializedIdentity.did, serializedIdentity)
-    debug('Created', serializedIdentity.did)
-    return this.identityFromSerialized(serializedIdentity)
+  async removeService(args: { did: string; id: string; options?: any }): Promise<any> {
+    throw Error('Not implemented')
   }
 
-  async deleteIdentity(did: string) {
-    const serializedIdentity = await this.identityStore.get(did)
-    for (const key of serializedIdentity.keys) {
-      await this.kms.deleteKey(key.kid)
-    }
-    return this.identityStore.delete(serializedIdentity.did)
-  }
-
-  async getIdentity(did: string) {
-    const serializedIdentity = await this.identityStore.get(did)
-    return this.identityFromSerialized(serializedIdentity)
-  }
-
-  async exportIdentity(did: string) {
-    const serializedIdentity = await this.identityStore.get(did)
-    return JSON.stringify(serializedIdentity)
-  }
-
-  async importIdentity(secret: string) {
-    const serializedIdentity: SerializedIdentity = JSON.parse(secret)
-    for (const serializedKey of serializedIdentity.keys) {
-      await this.kms.importKey(serializedKey)
-    }
-    await this.identityStore.set(serializedIdentity.did, serializedIdentity)
-    return this.identityFromSerialized(serializedIdentity)
-  }
 }
