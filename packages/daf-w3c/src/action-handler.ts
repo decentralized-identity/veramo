8505631078ed0bad6f5fbfedd675c2e43a04a974
Simonas Karuzas <simonas@not.cat>
2020-06-17 09:58:47 +0300

    fix: W3c tests

--- packages/daf-w3c/src/action-handler.ts
+++ packages/daf-w3c/src/action-handler.ts
@@ -1,4 +1,16 @@
-import { IAgentBase, IAgentResolve, IAgentIdentityManager, IAgentKeyManager, IAgentExtension, ICredential, IPresentation, IVerifiableCredential, IVerifiablePresentation, IAgentDataStore } from 'daf-core'
+import {
+  IAgentBase,
+  IAgentPlugin,
+  IAgentResolve,
+  IAgentIdentityManager,
+  IAgentKeyManager,
+  IAgentExtension,
+  ICredential,
+  IPresentation,
+  IVerifiableCredential,
+  IVerifiablePresentation,
+  IAgentDataStore,
+} from 'daf-core'
 import {
   createVerifiableCredential,
   createPresentation as createVerifiablePresentation,
@@ -7,128 +19,109 @@ import {
   verifyCredential,
 } from 'did-jwt-vc'
 import { decodeJWT } from 'did-jwt'
-import { Connection } from 'typeorm'
 
 import { createCredential, createPresentation } from './message-handler'
 
 import Debug from 'debug'
 const debug = Debug('daf:w3c:action-handler')
 
-export interface ISignPresentationJwtArgs {
-  data: PresentationInput
-  save?: boolean
+export interface IAgentW3c {
+  createVerifiablePresentation?: (args: {
+    presentation: IPresentation
+    save?: boolean
+    proofFormat: 'jwt'
+  }) => Promise<IVerifiablePresentation>
+  createVerifiableCredential?(args: {
+    credential: ICredential
+    save?: boolean
+    proofFormat: 'jwt'
+  }): Promise<IVerifiableCredential>
 }
-
-export interface ISignCredentialJwtArgs {
-  data: CredentialInput
-  save?: boolean
-}
-
-type TContext = {
+export interface IContext {
   agent: IAgentBase & IAgentIdentityManager & IAgentResolve & IAgentDataStore & IAgentKeyManager
-  dbConnection: Promise<Connection>
 }
 
-type TSignPresentationJwt = (args: ISignPresentationJwtArgs, context: TContext) => Promise<IPresentation>
-type TSignCredentialJwt = (args: ISignCredentialJwtArgs, context: TContext) => Promise<ICredential>
+export class W3c implements IAgentPlugin {
+  readonly methods: Required<IAgentW3c>
 
-export interface IAgentSignPresentationJwt {
-  signPresentationJwt?: IAgentExtension<TSignPresentationJwt>
-}
-
-export interface IAgentSignCredentialJwt {
-  signCredentialJwt?: IAgentExtension<TSignCredentialJwt>
-}
-
-export const signPresentationJwt: TSignPresentationJwt = async (args, context) => {
-  const { data, save } = args
-  try {
-    const payload = transformPresentationInput(data)
-    const identity = await context.agent.identityManagerGetIdentity({ did: data.issuer })
-    const key = identity.keys.find(k => k.type === 'Secp256k1')
-    const signer = (data: string) => context.agent.keyManagerSignJWT({kid: key.kid, data})
-    debug('Signing VP with', identity.did)
-    // Removing duplicate JWT
-    payload.vp.verifiableCredential = Array.from(new Set(payload.vp.verifiableCredential))
-    const jwt = await createVerifiablePresentation(payload, { did: identity.did, signer })
-
-    const credentials: IVerifiableCredential[] = []
-    for (const credentialJwt of payload.vp.verifiableCredential) {
-      const verified = await verifyCredential(credentialJwt, {
-        resolve: (didUrl: string) => context.agent.resolveDid({ didUrl }),
-      })
-      credentials.push(createCredential(verified.payload, credentialJwt))
+  constructor() {
+    this.methods = {
+      createVerifiablePresentation: this.createVerifiablePresentation,
+      createVerifiableCredential: this.createVerifiableCredential,
     }
+  }
 
-    debug(jwt)
-    const decoded = decodeJWT(jwt)
-    const presentation = createPresentation(decoded.payload as PresentationPayload, jwt, credentials)
-    if (save) {
-      await context.agent.dataStoreSaveVerifiablePresentation(presentation)
+  async createVerifiablePresentation(
+    args: {
+      presentation: IPresentation
+      save?: boolean
+      proofFormat: 'jwt'
+    },
+    context?: IContext,
+  ): Promise<IVerifiablePresentation> {
+    try {
+      const payload = transformPresentationInput(args.presentation)
+      const identity = await context.agent.identityManagerGetIdentity({ did: args.presentation.issuer })
+      const key = identity.keys.find(k => k.type === 'Secp256k1')
+      const signer = (data: string) => context.agent.keyManagerSignJWT({ kid: key.kid, data })
+      debug('Signing VP with', identity.did)
+      // Removing duplicate JWT
+      payload.vp.verifiableCredential = Array.from(new Set(payload.vp.verifiableCredential))
+      const jwt = await createVerifiablePresentation(payload, { did: identity.did, signer })
+
+      const credentials: IVerifiableCredential[] = []
+      for (const credentialJwt of payload.vp.verifiableCredential) {
+        const verified = await verifyCredential(credentialJwt, {
+          resolve: (didUrl: string) => context.agent.resolveDid({ didUrl }),
+        })
+        credentials.push(createCredential(verified.payload, credentialJwt))
+      }
+
+      debug(jwt)
+      const decoded = decodeJWT(jwt)
+      const presentation = createPresentation(decoded.payload as PresentationPayload, jwt, credentials)
+      if (args.save) {
+        await context.agent.dataStoreSaveVerifiablePresentation(presentation)
+      }
+      return presentation
+    } catch (error) {
+      debug(error)
+      return Promise.reject(error)
     }
-    return presentation
-  } catch (error) {
-    debug(error)
-    return Promise.reject(error)
+  }
+
+  async createVerifiableCredential(
+    args: {
+      credential: ICredential
+      save?: boolean
+      proofFormat: 'jwt'
+    },
+    context?: IContext,
+  ): Promise<IVerifiableCredential> {
+    try {
+      const payload = transformCredentialInput(args.credential)
+      const identity = await context.agent.identityManagerGetIdentity({ did: args.credential.issuer })
+      const key = identity.keys.find(k => k.type === 'Secp256k1')
+      const signer = (data: string) => context.agent.keyManagerSignJWT({ kid: key.kid, data })
+
+      debug('Signing VC with', identity.did)
+      const jwt = await createVerifiableCredential(payload, { did: identity.did, signer })
+      debug(jwt)
+      const decoded = decodeJWT(jwt)
+      const credential = createCredential(decoded.payload as VerifiableCredentialPayload, jwt)
+      if (args.save) {
+        await context.agent.dataStoreSaveVerifiableCredential(credential)
+      }
+
+      return credential
+    } catch (error) {
+      debug(error)
+      return Promise.reject(error)
+    }
   }
 }
 
-export const signCredentialJwt: TSignCredentialJwt = async (args, context) => {
-  const { data, save } = args
-  try {
-    const payload = transformCredentialInput(data)
-    const identity = await context.agent.identityManagerGetIdentity({ did: data.issuer })
-    const key = identity.keys.find(k => k.type === 'Secp256k1')
-    const signer = (data: string) => context.agent.keyManagerSignJWT({kid: key.kid, data})
-
-    debug('Signing VC with', identity.did)
-    const jwt = await createVerifiableCredential(payload, { did: identity.did, signer })
-    debug(jwt)
-    const decoded = decodeJWT(jwt)
-    const credential = createCredential(decoded.payload as VerifiableCredentialPayload, jwt)
-    if (save) {
-      await context.agent.dataStoreSaveVerifiableCredential(credential)
-    }
-    return credential
-  } catch (error) {
-    debug(error)
-    return Promise.reject(error)
-  }
-}
-
-export interface CredentialSubjectInput {
-  id: string
-  [x: string]: any
-}
-export interface CredentialInput {
-  '@context'?: string[]
-  context?: string[]
-  type: string[]
-  id?: string
-  issuer: string
-  expirationDate?: string
-  credentialSubject: CredentialSubjectInput
-  credentialStatus?: {
-    id: string
-    type: string
-  }
-  [x: string]: any
-}
-
-export interface PresentationInput {
-  '@context'?: string[]
-  context?: string[]
-  type: string[]
-  id?: string
-  issuer: string
-  audience: string[]
-  tag?: string
-  expirationDate?: string
-  verifiableCredential: string[]
-  [x: string]: any
-}
-
-const transformCredentialInput = (input: CredentialInput): VerifiableCredentialPayload => {
+const transformCredentialInput = (input: ICredential): VerifiableCredentialPayload => {
   if (Array.isArray(input.credentialSubject)) throw Error('credentialSubject of type array not supported')
 
   const result = { vc: {} }
@@ -168,7 +161,7 @@ const transformCredentialInput = (input: CredentialInput): VerifiableCredentialP
   return result as VerifiableCredentialPayload
 }
 
-const transformPresentationInput = (input: PresentationInput): PresentationPayload => {
+const transformPresentationInput = (input: IPresentation): PresentationPayload => {
   const result = { vp: {} }
 
   for (const key in input) {
