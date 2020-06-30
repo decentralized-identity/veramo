ae7190c707bcc4f6ce6094bfd2e827a02f7fb7be
Simonas Karuzas <simonas@not.cat>
2020-06-30 15:19:23 +0300

    feat: Express router

--- packages/daf-express/src/index.ts
+++ packages/daf-express/src/index.ts
@@ -1,21 +1,43 @@
 import { IAgent } from 'daf-core'
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction, Router, json } from 'express'
 import { supportedMethods } from 'daf-rest'
+import Debug from 'debug'
 
-export const AgentExpressMiddleware = (options: { agent: IAgent; methods: string[]; prefix: string }) => {
-  const availableEndpoints = options.methods.map(method => options.prefix + supportedMethods[method].path)
-  let methodPathMap: Record<string, string> = {}
-  for (const method of options.methods) {
-    methodPathMap[options.prefix + supportedMethods[method].path] = method
-  }
+interface RequestWithAgent extends Request {
+  agent?: IAgent
+}
 
-  return async (req: Request, res: Response, next: NextFunction) => {
-    if (availableEndpoints.includes(req.path)) {
-      const method = methodPathMap[req.path]
-      const result = await options.agent.execute(method, req.body)
-      res.json(result)
-    } else {
-      next()
+export const AgentRouter = (options: {
+  getAgentForRequest: (req: Request) => Promise<IAgent>
+  exposedMethods: string[]
+}) => {
+  const router = Router()
+  router.use(json())
+  router.use(async (req: RequestWithAgent, res, next) => {
+    req.agent = await options.getAgentForRequest(req)
+    next()
+  })
+
+  for (const exposedMethod of options.exposedMethods) {
+    const method = supportedMethods[exposedMethod]
+    if (!method) throw Error('Method not supported: ' + exposedMethod)
+    Debug('daf:express:initializing')(exposedMethod)
+
+    switch (method.type) {
+      case 'POST':
+        router.post(method.path, async (req: RequestWithAgent, res: Response, next: NextFunction) => {
+          if (!req.agent) throw Error('Agent not available')
+          try {
+            const result = await req.agent.execute(exposedMethod, req.body)
+            res.status(200).json(result)
+          } catch (e) {
+            res.status(500).json({ error: e.message })
+          }
+        })
+        break
+      // TODO: handle GET
     }
   }
+
+  return router
 }
