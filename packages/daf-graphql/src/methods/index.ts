10d31cc96b293ca0c2f48fa3e39cb94612146e55
Simonas Karuzas <simonas@not.cat>
2020-07-08 13:20:53 +0300

    feat: GraphQL message handler

--- packages/daf-graphql/src/methods/index.ts
+++ packages/daf-graphql/src/methods/index.ts
@@ -1,8 +1,10 @@
 import { IAgentGraphQLMethod } from '../types'
 import identityManager from './identity-manager'
 import resolver from './resolver'
+import messageHandler from './message-handler'
 
 export const supportedMethods: Record<string, IAgentGraphQLMethod> = {
   ...identityManager,
   ...resolver,
+  ...messageHandler,
 }
---
+++ packages/daf-graphql/src/methods/message-handler.ts
@@ -0,0 +1,42 @@
+import { IAgentGraphQLMethod } from '../types'
+
+export const handleMessage: IAgentGraphQLMethod = {
+  type: 'Mutation',
+  query: `
+    mutation handleMessage($raw: String!, $metaData: [MetaDataInput], $save: Boolean) {
+      handleMessage(raw: $raw, metaData: $metaData, save: $save) {
+        id
+        createdAt
+        expiresAt
+        threadId
+        type
+        raw
+        data
+        replyTo
+        replyUrl
+        from 
+        to 
+        metaData {
+          type
+          value
+        }
+      } 
+    }
+  `,
+  typeDef: `
+    input MetaDataInput {
+      type: String!
+      value: String
+    }
+
+    extend type Mutation {
+      handleMessage(raw: String!, metaData: [MetaDataInput], save: Boolean = true): Message
+    }
+  `,
+}
+
+export const supportedMethods: Record<string, IAgentGraphQLMethod> = {
+  handleMessage,
+}
+
+export default supportedMethods
