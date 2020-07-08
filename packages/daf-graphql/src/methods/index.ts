967b9168536e5c5a2ba484e3f912ba4661952f8f
Simonas Karuzas <simonas@not.cat>
2020-07-08 13:36:57 +0300

    feat: GraphQL w3c

--- packages/daf-graphql/src/methods/index.ts
+++ packages/daf-graphql/src/methods/index.ts
@@ -2,9 +2,11 @@ import { IAgentGraphQLMethod } from '../types'
 import identityManager from './identity-manager'
 import resolver from './resolver'
 import messageHandler from './message-handler'
+import w3c from './w3c'
 
 export const supportedMethods: Record<string, IAgentGraphQLMethod> = {
   ...identityManager,
   ...resolver,
   ...messageHandler,
+  ...w3c,
 }
