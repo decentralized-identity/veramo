56cf1412e9811f0fec675d9210e82614a6b15ea0
Simonas Karuzas <simonas@not.cat>
2020-07-24 14:21:31 +0300

    fix: Types for IW3c

--- packages/daf-w3c/src/index.ts
+++ packages/daf-w3c/src/index.ts
@@ -1,3 +1,8 @@
 import { IAgentPlugin, IPluginMethodMap } from 'daf-core'
 export { W3cMessageHandler, MessageTypes } from './message-handler'
-export { W3c, IW3c } from './action-handler'
+export {
+  W3c,
+  IW3c,
+  ICreateVerifiableCredentialArgs,
+  ICreateVerifiablePresentationArgs,
+} from './action-handler'
