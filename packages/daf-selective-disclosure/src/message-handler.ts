f04920fcc7e19839b39038f155d2941fc7163567
Simonas Karuzas <simonas@not.cat>
2020-09-07 18:01:28 +0300

    fix: SDR message handler replyTo

--- packages/daf-selective-disclosure/src/message-handler.ts
+++ packages/daf-selective-disclosure/src/message-handler.ts
@@ -25,7 +25,11 @@ export class SdrMessageHandler extends AbstractMessageHandler {
       message.type = MessageTypes.sdr
       message.from = message.data.iss
 
-      message.replyTo = Array.isArray(message.data.replyTo) ? message.data.replyTo : [message.data.replyTo]
+      message.replyTo = Array.isArray(message.data.replyTo)
+        ? message.data.replyTo
+        : message.data.replyTo
+        ? [message.data.replyTo]
+        : undefined
       message.replyUrl = message.data.replyUrl
 
       if (message.data.subject) {
