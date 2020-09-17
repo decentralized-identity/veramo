dbec78036118587e7a86dc1826d5c3c91e534069
Simonas Karuzas <simonas@not.cat>
2020-09-17 12:03:53 +0300

    feat: Daf-react-native-libsodium

---
+++ packages/daf-react-native-libsodium/README.md
@@ -0,0 +1,1 @@
+# DAF react native sodium modules
---
+++ packages/daf-react-native-libsodium/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Provides {@link https://github.com/jedisct1/libsodium.js | libsodium} backed {@link daf-libsodium#KeyManagementSystem | key management system } and {@link daf-libsodium#SecretBox | secret box } for the {@link daf-key-manager#KeyManager}
+ *
+ * @packageDocumentation
+ */
+export { KeyManagementSystem } from './key-management-system'
+export { SecretBox } from './secret-box'
