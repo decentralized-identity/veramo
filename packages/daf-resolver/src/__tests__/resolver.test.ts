ab9a32fbae06e0606aadb3b8637c7c456acd492e
Mircea Nistor <mirceanis@gmail.com>
2020-12-14 14:01:41 +0100

    chore(test): add placeholder tests to resolver

--- packages/daf-resolver/src/__tests__/resolver.test.ts
+++ packages/daf-resolver/src/__tests__/resolver.test.ts
@@ -10,7 +10,10 @@ describe('daf-resolver', () => {
       })
     }).toThrow()
   })
-
+it.todo('should resolve example did')
+it.todo('should fail predictably when unsupported method is resolved')
+it.todo('should resolve ethr-did with RPC URL')
+it.todo('should resolve ethr-did with custom web3 provider')
   it('should have resolve method', () => {
     const resolver = new DafResolver({ resolver: new Resolver() })
     expect(resolver).toHaveProperty('resolveDid')
