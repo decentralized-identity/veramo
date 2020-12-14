1cda340d59c2c97c68690935da4a0998c35f1712
Simonas Karuzas <simonas@not.cat>
2020-12-14 11:23:43 +0200

    chore: Removing daf-resolver-universal package

--- packages/daf-resolver-universal/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Provides a {@link daf-resolver-universal#DafUniversalResolver | plugin} for the {@link daf-core#Agent} that implements  {@link daf-core#IResolver } interface. Uses external "universal" resolver
- *
- * @packageDocumentation
- */
-export { DafUniversalResolver } from './resolver'
