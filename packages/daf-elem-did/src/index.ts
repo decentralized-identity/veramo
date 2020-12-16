b6a19322dc2cdf7658c8274678f198e58b0f3505
Mircea Nistor <mirceanis@gmail.com>
2020-12-16 18:59:59 +0100

    chore: Remove daf-elem-did package

--- packages/daf-elem-did/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Provides `did:elem` {@link daf-elem-did#ElemIdentityProvider | identity provider } for the {@link daf-identity-manager#IdentityManager}
- *
- * @packageDocumentation
- */
-export { ElemIdentityProvider } from './identity-provider'
