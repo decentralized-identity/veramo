3ec73b96de0857543ee87344ed3d741a04cfdcdb
Mircea Nistor <mirceanis@gmail.com>
2020-12-17 17:54:53 +0100

    chore(ci): refactor automatic release using GH actions (#283)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'always', ['sentence-case']],
+    //    'subject-case': [2, 'always', ['sentence-case']],
   },
 }
