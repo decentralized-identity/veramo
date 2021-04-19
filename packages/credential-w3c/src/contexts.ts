b206074c6d48aab275ffbd8182b725f22a89e2e3
Martin Riedel <web@riedel-privat.de>
2021-04-19 09:39:40 -0500

    fix: fixed loading of context files from build.

--- packages/credential-w3c/src/contexts.ts
+++ packages/credential-w3c/src/contexts.ts
@@ -5,15 +5,15 @@ import * as path from 'path';
 function _read(_path: string) {
   return JSON.parse(
     fs.readFileSync(
-      path.join(__dirname, _path),
+      path.join(__dirname, '../contexts', _path),
       {encoding: 'utf8'}));
 }
 
 const contexts = new Map([
-  ['https://veramo.io/contexts/socialmedia/v1', _read('./contexts/socialmedia-v1.jsonld')],
-  ['https://veramo.io/contexts/kyc/v1', _read('./contexts/kyc-v1.jsonld')],
-  ['https://www.w3.org/ns/did/v1', _read('./contexts/security_context_v1.jsonld')],
-  ['https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld', _read('./contexts/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld')]
+  ['https://veramo.io/contexts/socialmedia/v1', _read('socialmedia-v1.jsonld')],
+  ['https://veramo.io/contexts/kyc/v1', _read('kyc-v1.jsonld')],
+  ['https://www.w3.org/ns/did/v1', _read('security_context_v1.jsonld')],
+  ['https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld', _read('lds-ecdsa-secp256k1-recovery2020-0.0.jsonld')]
 ]);
 
 export default contexts;
