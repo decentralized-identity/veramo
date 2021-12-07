d6122d33cff350871bcb5a232fb0abd2ab1c7631
Simonas Karuzas <simonas@not.cat>
2021-12-07 12:58:54 +0200

    fix(remote-server): api-key-auth

--- packages/remote-server/src/api-key-auth.ts
+++ packages/remote-server/src/api-key-auth.ts
@@ -1,7 +1,10 @@
 import passport from 'passport'
 import Bearer from 'passport-http-bearer'
+import { Router } from 'express'
 
 export function apiKeyAuth({ apiKey }: { apiKey: string }) {
+  const router = Router()
+  router.use(passport.initialize())
   passport.use(
     new Bearer.Strategy((token, done) => {
       if (!apiKey || apiKey === token) {
@@ -11,5 +14,6 @@ export function apiKeyAuth({ apiKey }: { apiKey: string }) {
       }
     }),
   )
-  return passport.authenticate('bearer', { session: false })
+  router.use(passport.authenticate('bearer', { session: false }))
+  return router
 }
