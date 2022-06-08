020dcba7471b8446efe40c86a345e31636e14d1e
Mircea Nistor <mirceanis@gmail.com>
2022-06-08 15:20:25 +0200

    docs(did-discovery): update and fix inline docs for @veramo/did-discovery exports

--- packages/did-discovery/src/index.ts
+++ packages/did-discovery/src/index.ts
@@ -6,5 +6,11 @@
 export { DIDDiscovery } from './action-handler'
 export { AbstractDidDiscoveryProvider } from './abstract-did-discovery-provider'
 export * from './types'
+
+/**
+ * The parameter and return type schemas for the methods of the {@link @veramo/did-discovery#DIDDiscovery} plugin.
+ *
+ * @beta This API may change without a BREAKING CHANGE notice.
+ */
 const schema = require('../plugin.schema.json')
 export { schema }
