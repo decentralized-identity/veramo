1466b2465333e3a25fb6dce0d6ac835076a181de
Mircea Nistor <mirceanis@gmail.com>
2022-06-12 16:39:47 +0200

    fix(build): adjust browser-tests for jest v28
    * force jsonld to resolve to @digitalbazaar/jsonld
    * run browser tests with `NODE_OPTIONS=--experimental-vm-modules`
    * explicitly install some jest packages

--- packages/test-react-app/jest-integration.config.js
+++ packages/test-react-app/jest-integration.config.js
@@ -2,9 +2,11 @@ module.exports = {
   preset: 'jest-puppeteer',
   rootDir: 'headless-tests',
   testRegex: './*\\.browser-test\\.(ts|tsx?)$',
-  globalSetup: 'jest-environment-puppeteer/setup',
-  globalTeardown: 'jest-environment-puppeteer/teardown',
-  testEnvironment: 'jest-environment-puppeteer',
+  transform: {"\\.ts$": ['ts-jest']},
   testTimeout: 3 * 60 * 1000,
   testRunner: 'jest-jasmine2',
+  moduleNameMapper: {
+    // jest 28 loads modules differently. See https://jestjs.io/docs/upgrading-to-jest28#packagejson-exports
+    '^uuid$': require.resolve('uuid'),
+  }
 }
