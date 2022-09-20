1d69b7ebc479d90f8add51dfc2f59d82f4ef9bb3
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-09-20 14:15:27 -0400

    chore: instantiate libp2p node in localAgent test

--- __tests__/localAgent.test.ts
+++ __tests__/localAgent.test.ts
@@ -139,37 +139,37 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
   const { provider, registry } = await createGanacheProvider()
   const ethersProvider = createEthersProvider()
 
-  // const webRtcStar = new WebRTCStar()
-  // const libp2p = await createLibp2p({
-  //   addresses: {
-  //     // Add the signaling server address, along with our PeerId to our multiaddrs list
-  //     // libp2p will automatically attempt to dial to the signaling server so that it can
-  //     // receive inbound connections from other peers
-  //     listen: [
-  //       '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
-  //       '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
-  //     ]
-  //   },
-  //   transports: [
-  //     new WebSockets(),
-  //     webRtcStar
-  //   ],
-  //   connectionEncryption: [new Noise()],
-  //   streamMuxers: [new Mplex()],
-  //   peerDiscovery: [
-  //     webRtcStar.discovery,
-  //     new Bootstrap({
-  //       list: [
-  //         '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-  //         '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-  //         '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
-  //         '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-  //         '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
-  //       ]
-  //     })
-  //   ]
-  // })
-  // console.log("libp2p: ", libp2p)
+  const webRtcStar = new WebRTCStar()
+  const libp2p = await createLibp2p({
+    addresses: {
+      // Add the signaling server address, along with our PeerId to our multiaddrs list
+      // libp2p will automatically attempt to dial to the signaling server so that it can
+      // receive inbound connections from other peers
+      listen: [
+        '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+      ]
+    },
+    transports: [
+      new WebSockets(),
+      webRtcStar
+    ],
+    connectionEncryption: [new Noise()],
+    streamMuxers: [new Mplex()],
+    peerDiscovery: [
+      webRtcStar.discovery,
+      new Bootstrap({
+        list: [
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+        ]
+      })
+    ]
+  })
+  console.log("libp2p: ", libp2p)
 
   agent = createAgent<
     IDIDManager &
