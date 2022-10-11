a8b960b41494ce249a723cf3e9269df3816666d8
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-10-10 23:46:04 -0400

    fix: remove deprecated testnets

--- __tests__/localJsonStoreAgent.test.ts
+++ __tests__/localJsonStoreAgent.test.ts
@@ -129,7 +129,7 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
       }),
       new DIDManager({
         store: new DIDStoreJson(jsonFileStore),
-        defaultProvider: 'did:ethr:rinkeby',
+        defaultProvider: 'did:ethr:goerli',
         providers: {
           'did:ethr': new EthrDIDProvider({
             defaultKms: 'local',
@@ -140,14 +140,14 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
                 rpcUrl: 'https://mainnet.infura.io/v3/' + infuraProjectId,
               },
               {
-                name: 'rinkeby',
-                rpcUrl: 'https://rinkeby.infura.io/v3/' + infuraProjectId,
+                name: 'goerli',
+                rpcUrl: 'https://goerli.infura.io/v3/' + infuraProjectId,
               },
               {
-                chainId: 421611,
-                name: 'arbitrum:rinkeby',
-                rpcUrl: 'https://arbitrum-rinkeby.infura.io/v3/' + infuraProjectId,
-                registry: '0x8f54f62CA28D481c3C30b1914b52ef935C1dF820',
+                chainId: 421613,
+                name: 'arbitrum:goerli',
+                rpcUrl: 'https://arbitrum-goerli.infura.io/v3/' + infuraProjectId,
+                registry: '0x8FFfcD6a85D29E9C33517aaf60b16FE4548f517E',
               },
             ],
           }),
