fb22e632ef6dcce6a7dfec9a229c7be4d6d5c894
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-12-05 21:13:26 -0500

    feat(did-comm): add trust ping protocol (#1080)
    * feat(did-comm): add trust ping protocol
    
    * chore: cleanup
    
    * chore: more test
    
    * chore: cleanup
    
    * chore: refactor test slightly
    
    * chore: stash test scratch work
    
    * fix: update jsonld resolution
    
    * chore: cleanup and fixing tests
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: remove jsonld resolution
    
    * chore: update README
    
    * chore: PR feedback
    
    * chore: update default agent config
    
    Co-authored-by: Nick Reynolds <nick@joincivil.com>

---
+++ packages/did-comm/src/protocols/trust-ping-message-handler.ts
@@ -0,0 +1,82 @@
+import { IAgentContext, IDIDManager, IKeyManager } from '@veramo/core'
+import { AbstractMessageHandler, Message } from '@veramo/message-handler'
+import Debug from 'debug'
+import { v4 } from 'uuid'
+import { IDIDComm } from '../types/IDIDComm'
+import { IDIDCommMessage } from '../types/message-types'
+
+const debug = Debug('veramo:did-comm:trust-ping-message-handler')
+
+type IContext = IAgentContext<IDIDManager & IKeyManager & IDIDComm>
+
+const TRUST_PING_MESSAGE_TYPE = 'https://didcomm.org/trust-ping/2.0/ping'
+const TRUST_PING_RESPONSE_MESSAGE_TYPE = 'https://didcomm.org/trust-ping/2.0/ping-response'
+
+export function createTrustPingMessage(senderDidUrl: string, recipientDidUrl: string): IDIDCommMessage {
+  return {
+    type: TRUST_PING_MESSAGE_TYPE,
+    from: senderDidUrl,
+    to: recipientDidUrl,
+    id: v4(),
+    body: {
+      responseRequested: true
+    }
+  }
+}
+
+export function createTrustPingResponse(senderDidUrl: string, recipientDidUrl: string, pingId: string): IDIDCommMessage {
+  return {
+    type: TRUST_PING_RESPONSE_MESSAGE_TYPE,
+    from: senderDidUrl,
+    to: recipientDidUrl,
+    id: `${pingId}-response`,
+    thid: pingId,
+    body: {}
+  }
+}
+
+/**
+ * A plugin for the {@link @veramo/message-handler#MessageHandler} that handles Trust Ping messages.
+ * @beta This API may change without a BREAKING CHANGE notice.
+ */
+export class TrustPingMessageHandler extends AbstractMessageHandler {
+  constructor() {
+    super()
+  }
+
+  /**
+   * Handles a Trust Ping Message
+   * https://identity.foundation/didcomm-messaging/spec/#trust-ping-protocol-10
+   */
+  public async handle(message: Message, context: IContext): Promise<Message> {
+    if (message.type === TRUST_PING_MESSAGE_TYPE) {
+      debug('TrustPing Message Received')
+      try {
+        const { from, to, id } = message
+        if (!from) {
+          throw new Error("invalid_argument: Trust Ping Message received without `from` set")
+        }
+        if (!to) {
+          throw new Error("invalid_argument: Trust Ping Message received without `to` set")
+        }
+        const response = createTrustPingResponse(to!, from!, id)
+        const packedResponse = await context.agent.packDIDCommMessage({ message: response, packing: 'authcrypt'})
+        const sent = await context.agent.sendDIDCommMessage({
+          messageId: response.id,
+          packedMessage: packedResponse,
+          recipientDidUrl: from!,
+        })
+        message.addMetaData({ type: 'TrustPingResponseSent', value: sent })
+      } catch (ex) {
+        debug(ex)
+      }
+      return message
+    } else if (message.type === TRUST_PING_RESPONSE_MESSAGE_TYPE) {
+      debug('TrustPingResponse Message Received')
+      message.addMetaData({ type: 'TrustPingResponseReceived', value: 'true'})
+      return message
+    }
+
+    return super.handle(message, context)
+  }
+}
