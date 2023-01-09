7311e42bb25f8c5174cc61f4782d98fc75bd6afd
Cody Hatfield <cody.hatfield@me.com>
2023-01-09 15:12:04 -0800

    feat(did-comm) Recipient handles MediateGrant

--- packages/did-comm/src/protocols/coordinate-mediation-message-handler.ts
+++ packages/did-comm/src/protocols/coordinate-mediation-message-handler.ts
@@ -1,4 +1,4 @@
-import { IAgentContext, IDIDManager, IKeyManager } from '@veramo/core'
+import { IAgentContext, IDIDManager, IKeyManager, IDataStore } from '@veramo/core'
 import { AbstractMessageHandler, Message } from '@veramo/message-handler'
 import Debug from 'debug'
 import { v4 } from 'uuid'
@@ -7,37 +7,41 @@ import { IDIDCommMessage, DIDCommMessageMediaType } from '../types/message-types
 
 const debug = Debug('veramo:did-comm:trust-ping-message-handler')
 
-type IContext = IAgentContext<IDIDManager & IKeyManager & IDIDComm>
+type IContext = IAgentContext<IDIDManager & IKeyManager & IDIDComm & IDataStore>
 
 const MEDIATE_REQUEST_MESSAGE_TYPE = 'https://didcomm.org/coordinate-mediation/2.0/mediate-request'
 const MEDIATE_GRANT_MESSAGE_TYPE = 'https://didcomm.org/coordinate-mediation/2.0/mediate-grant'
 const MEDIATE_DENY_MESSAGE_TYPE = 'https://didcomm.org/coordinate-mediation/2.0/mediate-deny'
 
 export function createMediateRequestMessage(
-	recipientDidUrl: string,
-	mediatorDidUrl: string,
+  recipientDidUrl: string,
+  mediatorDidUrl: string,
 ): IDIDCommMessage {
-	return {
-		type: MEDIATE_REQUEST_MESSAGE_TYPE,
-		from: recipientDidUrl,
-		to: mediatorDidUrl,
-		id: v4(),
-		return_route: "all",
-		body: {},
-	}
+  return {
+    type: MEDIATE_REQUEST_MESSAGE_TYPE,
+    from: recipientDidUrl,
+    to: mediatorDidUrl,
+    id: v4(),
+    return_route: 'all',
+    body: {},
+  }
 }
 
-export function createMediateGrantMessage(recipientDidUrl: string, mediatorDidUrl: string, thid: string): IDIDCommMessage {
-	return {
-		type: MEDIATE_GRANT_MESSAGE_TYPE,
-		from: mediatorDidUrl,
-		to: recipientDidUrl,
-		id: v4(),
-		thid: thid,
-		body: {
-			routing_did: [mediatorDidUrl],
-		},
-	}
+export function createMediateGrantMessage(
+  recipientDidUrl: string,
+  mediatorDidUrl: string,
+  thid: string,
+): IDIDCommMessage {
+  return {
+    type: MEDIATE_GRANT_MESSAGE_TYPE,
+    from: mediatorDidUrl,
+    to: recipientDidUrl,
+    id: v4(),
+    thid: thid,
+    body: {
+      routing_did: [mediatorDidUrl],
+    },
+  }
 }
 
 /**
@@ -45,46 +49,104 @@ export function createMediateGrantMessage(recipientDidUrl: string, mediatorDidUr
  * @beta This API may change without a BREAKING CHANGE notice.
  */
 export class CoordinateMediationMediatorMessageHandler extends AbstractMessageHandler {
-	constructor() {
-		super()
-	}
+  constructor() {
+    super()
+  }
 
-	/**
-	 * Handles a Mediator Coordinator messages for the mediator role
-	 * https://didcomm.org/mediator-coordination/2.0/
-	 */
-	public async handle(message: Message, context: IContext): Promise<Message> {
-		if (message.type === MEDIATE_REQUEST_MESSAGE_TYPE) {
-			debug('MediateRequest Message Received')
-			try {
-				const { from, to, returnRoute } = message
-				if (!from) {
-					throw new Error('invalid_argument: MediateRequest received without `from` set')
-				}
-				if (!to) {
-					throw new Error('invalid_argument: MediateRequest received without `to` set')
-				}
-				if (returnRoute === 'all') {
-					// Grant requests to all recipients
-					// TODO: Come up with another method for approving and rejecting recipients
-					const response = createMediateGrantMessage(from, to, message.id)
-					const packedResponse = await context.agent.packDIDCommMessage({
-						message: response,
-						packing: 'authcrypt',
-					})
-					const returnResponse = {
-						id: response.id,
-						message: packedResponse.message,
-						contentType: DIDCommMessageMediaType.ENCRYPTED,
-					}
-					message.addMetaData({ type: 'ReturnRouteResponse', value: JSON.stringify(returnResponse) })
-				}
-			} catch (ex) {
-				debug(ex)
-			}
-			return message
-		}
+  /**
+   * Handles a Mediator Coordinator messages for the mediator role
+   * https://didcomm.org/mediator-coordination/2.0/
+   */
+  public async handle(message: Message, context: IContext): Promise<Message> {
+    if (message.type === MEDIATE_REQUEST_MESSAGE_TYPE) {
+      debug('MediateRequest Message Received')
+      try {
+        const { from, to, returnRoute } = message
+        if (!from) {
+          throw new Error('invalid_argument: MediateRequest received without `from` set')
+        }
+        if (!to) {
+          throw new Error('invalid_argument: MediateRequest received without `to` set')
+        }
+        if (returnRoute === 'all') {
+          // Grant requests to all recipients
+          // TODO: Come up with another method for approving and rejecting recipients
+          const response = createMediateGrantMessage(from, to, message.id)
+          const packedResponse = await context.agent.packDIDCommMessage({
+            message: response,
+            packing: 'authcrypt',
+          })
+          const returnResponse = {
+            id: response.id,
+            message: packedResponse.message,
+            contentType: DIDCommMessageMediaType.ENCRYPTED,
+          }
+          message.addMetaData({ type: 'ReturnRouteResponse', value: JSON.stringify(returnResponse) })
+        }
+      } catch (ex) {
+        debug(ex)
+      }
+      return message
+    }
 
-		return super.handle(message, context)
-	}
+    return super.handle(message, context)
+  }
+}
+
+/**
+ * A plugin for the {@link @veramo/message-handler#MessageHandler} that handles Mediator Coordinator messages for the recipient role.
+ * @beta This API may change without a BREAKING CHANGE notice.
+ */
+export class CoordinateMediationRecipientMessageHandler extends AbstractMessageHandler {
+  constructor() {
+    super()
+  }
+
+  /**
+   * Handles a Mediator Coordinator messages for the recipient role
+   * https://didcomm.org/mediator-coordination/2.0/
+   */
+  public async handle(message: Message, context: IContext): Promise<Message> {
+    if (message.type === MEDIATE_GRANT_MESSAGE_TYPE) {
+      debug('MediateGrant Message Received')
+      try {
+        const { from, to, data, threadId } = message
+        if (!from) {
+          throw new Error('invalid_argument: MediateGrant received without `from` set')
+        }
+        if (!to) {
+          throw new Error('invalid_argument: MediateGrant received without `to` set')
+        }
+        if (!threadId) {
+          throw new Error('invalid_argument: MediateGrant received without `thid` set')
+        }
+        if (!data.routing_did || data.routing_did.length === 0) {
+          throw new Error('invalid_argument: MediateGrant received with invalid routing_did')
+        }
+        // If mediate request was previously sent, add service to DID document
+        const prevRequestMsg = await context.agent.dataStoreGetMessage({ id: threadId })
+        if (prevRequestMsg.from === to && prevRequestMsg.to === from) {
+          const service = {
+            id: 'didcomm-mediator',
+            type: 'DIDCommMessaging',
+            serviceEndpoint: [
+              {
+                uri: data.routing_did[0],
+              },
+            ],
+          }
+          await context.agent.didManagerAddService({
+            did: to,
+            service: service,
+          })
+          message.addMetaData({ type: 'DIDCommMessagingServiceAdded', value: JSON.stringify(service) })
+        }
+      } catch (ex) {
+        debug(ex)
+      }
+      return message
+    }
+
+    return super.handle(message, context)
+  }
 }
