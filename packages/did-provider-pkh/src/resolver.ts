42be48ffe2251510f7bd5e10b43362e816655eb9
dchagastelles <dchagastelles@gmail.com>
2023-02-07 14:27:41 -0500

    fix(did-provider-pkh): refactor and simplify did:pkh plugin (#1113)

--- packages/did-provider-pkh/src/resolver.ts
+++ packages/did-provider-pkh/src/resolver.ts
@@ -1,21 +1,22 @@
-import { AccountId, ChainIdParams } from 'caip'
+import { AccountId, ChainIdParams } from 'caip';
 import type {
-  DIDResolutionResult,
   DIDResolutionOptions,
-  ResolverRegistry,
+  DIDResolutionResult,
   ParsedDID,
-  Resolvable
-} from 'did-resolver'
+  Resolvable,
+  ResolverRegistry,
+} from 'did-resolver';
+import { isValidNamespace, SECPK1_NAMESPACES } from './pkh-did-provider';
+import Debug from 'debug'
 
-const DID_LD_JSON = 'application/did+ld+json'
-const DID_JSON = 'application/did+json'
-const SECPK1_NAMESPACES = ['eip155', 'bip122']
-const TZ_NAMESPACE = 'tezos'
+const debug = Debug('veramo:pkh-did-resolver')
+const DID_LD_JSON = 'application/did+ld+json';
+const DID_JSON = 'application/did+json';
 
-
-function toDidDoc(did: string, accountId: string): any {
-  const { namespace } = AccountId.parse(accountId).chainId as ChainIdParams
-  const vmId = did + '#blockchainAccountId'
+function toDidDoc(did: string, blockchainAccountId: string): any {
+  const { namespace } = AccountId.parse(blockchainAccountId)
+    .chainId as ChainIdParams;
+  const vmId = did + '#blockchainAccountId';
   const doc = {
     '@context': [
       'https://www.w3.org/ns/did/v1',
@@ -23,7 +24,6 @@ function toDidDoc(did: string, accountId: string): any {
         blockchainAccountId: 'https://w3id.org/security#blockchainAccountId',
         EcdsaSecp256k1RecoveryMethod2020:
           'https://identity.foundation/EcdsaSecp256k1RecoverySignature2020#EcdsaSecp256k1RecoveryMethod2020',
-        Ed25519VerificationKey2018: 'https://w3id.org/security#Ed25519VerificationKey2018',
       },
     ],
     id: did,
@@ -32,29 +32,21 @@ function toDidDoc(did: string, accountId: string): any {
         id: vmId,
         type: 'EcdsaSecp256k1RecoveryMethod2020',
         controller: did,
-        blockchainAccountId: accountId,
+        blockchainAccountId,
       },
     ],
     authentication: [vmId],
     assertionMethod: [vmId],
+  };
+  if (!isValidNamespace(namespace)) {
+    debug(
+      `Invalid namespace '${namespace}'. Valid namespaces are: ${SECPK1_NAMESPACES}`
+    );
+    throw new Error(
+      `illegal_argument: namespace '${namespace}' not supported. Valid namespaces are: ${SECPK1_NAMESPACES}`
+    );
   }
-  if (SECPK1_NAMESPACES.includes(namespace)) {
-    // nothing to do here
-  } else if (namespace === TZ_NAMESPACE) {
-    (doc['@context'][1] as any).TezosMethod2021 = 'https://w3id.org/security#TezosMethod2021'
-    const tzId = did + '#TezosMethod2021'
-    doc.verificationMethod.push({
-      id: tzId,
-      type: 'TezosMethod2021',
-      controller: did,
-      blockchainAccountId: accountId,
-    })
-    doc.authentication.push(tzId)
-    doc.assertionMethod.push(tzId)
-  } else {
-    throw new Error(`chain namespace not supported ${namespace}`)
-  }
-  return doc
+  return doc;
 }
 
 export function getResolver(): ResolverRegistry {
@@ -65,28 +57,28 @@ export function getResolver(): ResolverRegistry {
       r: Resolvable,
       options: DIDResolutionOptions
     ): Promise<DIDResolutionResult> => {
-      const contentType = options.accept || DID_JSON
+      const contentType = options.accept || DID_JSON;
       const response: DIDResolutionResult = {
         didResolutionMetadata: { contentType },
         didDocument: null,
         didDocumentMetadata: {},
-      }
+      };
       try {
-        const doc = toDidDoc(did, parsed.id)
+        const doc = toDidDoc(did, parsed.id);
         if (contentType === DID_LD_JSON) {
-          response.didDocument = doc
+          response.didDocument = doc;
         } else if (contentType === DID_JSON) {
-          delete doc['@context']
-          response.didDocument = doc
+          delete doc['@context'];
+          response.didDocument = doc;
         } else {
-          delete response.didResolutionMetadata.contentType
-          response.didResolutionMetadata.error = 'representationNotSupported'
+          delete response.didResolutionMetadata.contentType;
+          response.didResolutionMetadata.error = 'representationNotSupported';
         }
       } catch (e) {
-        response.didResolutionMetadata.error = 'invalidDid'
-        response.didResolutionMetadata.message = e.toString()
+        response.didResolutionMetadata.error = 'invalidDid';
+        response.didResolutionMetadata.message = (e as Error).message;
       }
-      return response
+      return response;
     },
-  }
+  };
 }
