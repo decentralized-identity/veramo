11ee872b2de82f14ae57f80f07e1db405573e28d
nickreynolds <nicholas.s.reynolds@gmail.com>
2023-02-11 17:54:10 -0500

    fix: correctly export PickupRecipientMessageHandler

--- packages/did-comm/src/protocols/index.ts
+++ packages/did-comm/src/protocols/index.ts
@@ -1,4 +1,4 @@
 export { TrustPingMessageHandler } from './trust-ping-message-handler.js'
 export { CoordinateMediationMediatorMessageHandler, CoordinateMediationRecipientMessageHandler } from "./coordinate-mediation-message-handler.js"
 export { RoutingMessageHandler } from "./routing-message-handler.js"
-export { PickupMediatorMessageHandler } from "./messagepickup-message-handler.js"
+export { PickupMediatorMessageHandler, PickupRecipientMessageHandler } from "./messagepickup-message-handler.js"
