fc0aca4c77db17609050412586a94c7df238a733
Mircea Nistor <mirceanis@gmail.com>
2023-02-23 14:38:50 +0100

    feat(cli): export configuration creator methods and document them

--- packages/cli/src/lib/objectCreator.ts
+++ packages/cli/src/lib/objectCreator.ts
@@ -2,6 +2,50 @@ import { set, get } from 'jsonpointer'
 import parse from 'url-parse'
 import { resolve } from 'path'
 
+/**
+ * Creates objects from a configuration object and a set of pointers.
+ *
+ * Example:
+ * ```ts
+ * const { url } = createObjects({ "rpcUrl": "http://localhost:8545", }, { url: '/rpcUrl' })
+ * ```
+ *
+ * The config can contain references (`$ref`) to other objects within using JSON pointers.
+ * Example:
+ * ```json5
+ * {
+ *   rpcUrl: "http://localhost:8545",
+ *   endpoint: {
+ *     url: {
+ *       "$ref": "/rpcUrl"
+ *     }
+ *   }
+ * }
+ *
+ * The config object can also contain references to NPM modules using the `$require` property.
+ * Example:
+ * ```json5
+ * {
+ *   agent: {
+ *     "$require": "@veramo/core#Agent",
+ *     "$args": {
+ *       plugins: [
+ *         { "$require": "@veramo/did-comm#DIDComm" },
+ *       ]
+ *     }
+ *   }
+ * }
+ * ```
+ * Environment variables can also be specified using the `$env` property.
+ *
+ * @see Please see {@link https://veramo.io/docs/veramo_agent/configuration_internals | Configuration Internals} for
+ *   more information.
+ *
+ * @param config - The configuration object
+ * @param pointers - A map of JSON pointers to objects within that config that you wish to create
+ *
+ * @beta
+ */
 export async function createObjects(config: object, pointers: Record<string, string>): Promise<any> {
   const objects = {}
 
@@ -51,9 +95,9 @@ export async function createObjects(config: object, pointers: Record<string, str
       module = resolve(module)
     }
 
-    const resolvedArgs = args !== undefined ? (await resolveRefs(args)) : []
+    const resolvedArgs = args !== undefined ? await resolveRefs(args) : []
     try {
-      let required = member ? (await import(module))[member] : (await import(module))
+      let required = member ? (await import(module))[member] : await import(module)
       if (type === 'class') {
         object = new required(...resolvedArgs)
       } else if (type === 'function') {
