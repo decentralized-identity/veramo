427ec909750148f4df65964ecbea975e72f4075a
Mircea Nistor <mirceanis@gmail.com>
2023-05-18 18:18:14 +0200

    fix(cli): prevent `ExperimentalWarning` messages form being printed

--- packages/cli/bin/veramo.js
+++ packages/cli/bin/veramo.js
@@ -1,3 +1,16 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
+const originalEmit = process.emit;
+process.emit = function (name, data, ...args) {
+  if (
+    name === `warning` &&
+    typeof data === `object` &&
+    data.name === `ExperimentalWarning`
+    //if you want to only stop certain messages, test for the message here:
+    //&& data.message.includes(`Fetch API`)
+  ) {
+    return false;
+  }
+  return originalEmit.apply(process, arguments);
+};
 
 import '../build/cli.js'
