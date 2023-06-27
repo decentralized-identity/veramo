55e7e555fff889bd52ff002fc26d9fa36bd0be22
Martin Domajnko <domajnko.martin@gmail.com>
2023-06-27 15:40:17 +0200

    feat: allow secp256r1 key type

--- packages/credential-w3c/src/action-handler.ts
+++ packages/credential-w3c/src/action-handler.ts
@@ -130,7 +130,7 @@ export class CredentialPlugin implements IAgentPlugin {
       throw new Error('invalid_argument: presentation.holder must be a DID managed by this agent')
     }
     //FIXME: `args` should allow picking a key or key type
-    const key = identifier.keys.find((k) => k.type === 'Secp256k1' || k.type === 'Ed25519')
+    const key = identifier.keys.find((k) => k.type === 'Secp256k1' || k.type === 'Ed25519' || k.type === 'Secp256r1')
     if (!key) throw Error('key_not_found: No signing key for ' + identifier.did)
 
     let verifiablePresentation: VerifiablePresentation
@@ -165,9 +165,11 @@ export class CredentialPlugin implements IAgentPlugin {
       let alg = 'ES256K'
       if (key.type === 'Ed25519') {
         alg = 'EdDSA'
+      } else if (key.type === 'Secp256r1') {
+        alg = 'ES256'
       }
-      const signer = wrapSigner(context, key, alg)
 
+      const signer = wrapSigner(context, key, alg)
       const jwt = await createVerifiablePresentationJwt(
         presentation as any,
         { did: identifier.did, signer, alg },
@@ -236,14 +238,17 @@ export class CredentialPlugin implements IAgentPlugin {
         }
       } else {
         //FIXME: `args` should allow picking a key or key type
-        const key = identifier.keys.find((k) => k.type === 'Secp256k1' || k.type === 'Ed25519')
+        const key = identifier.keys.find((k) => k.type === 'Secp256k1' || k.type === 'Ed25519' || k.type === 'Secp256r1')
         if (!key) throw Error('No signing key for ' + identifier.did)
 
         debug('Signing VC with', identifier.did)
         let alg = 'ES256K'
         if (key.type === 'Ed25519') {
           alg = 'EdDSA'
+        } else if (key.type === 'Secp256r1') {
+          alg = 'ES256'
         }
+
         const signer = wrapSigner(context, key, alg)
         const jwt = await createVerifiableCredentialJwt(
           credential as any,
