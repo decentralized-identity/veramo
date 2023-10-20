1852834ee0a664f81ab537ff3a27ff74d7719b61
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-10-20 13:19:14 +0700

    feat: add "mediate" cli command for allowing or denying dids for mediation

---
+++ packages/cli/src/mediate.ts
@@ -0,0 +1,38 @@
+import { Command } from 'commander'
+import { getAgent } from './setup.js'
+
+import { MediationPolicies } from '@veramo/core-types'
+
+const ALLOW = MediationPolicies.ALLOW
+const DENY = MediationPolicies.DENY
+
+type Options = {
+  allow?: string
+  deny?: string
+}
+
+const parseStringToList = (list?: string): string[] => {
+  if (!list) return []
+  return list.split(' ').map((item) => item.trim())
+}
+
+const handleAction = async ({ allow, deny }: Options, cmd: Command) => {
+  try {
+    const agent = await getAgent(cmd.optsWithGlobals().config)
+    const didsToAllow = parseStringToList(allow)
+    const didsToDeny = parseStringToList(deny)
+    didsToAllow.forEach(async (did) => await agent.dataStoreSaveMediationPolicy({ did, policy: ALLOW }))
+    didsToDeny.forEach(async (did) => await agent.dataStoreSaveMediationPolicy({ did, policy: DENY }))
+    console.dir({ didsToAllow, didsToDeny })
+  } catch (e) {
+    console.error(e.message)
+  }
+}
+
+const mediate = new Command('mediate')
+  .description('Mediate allow or deny policy on dids')
+  .option('-a, --allow <string>', 'string containing a list of dids to allow')
+  .option('-d, --deny <string>', 'string containing a list of dids to deny')
+  .action(handleAction)
+
+export { mediate }
