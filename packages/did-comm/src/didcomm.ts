6facf7ac394f177b3eae4b0703255acf18e0f759
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-10-25 11:29:41 +0700

    refactor: pass DIDComm config from agent.yml as object

--- packages/did-comm/src/didcomm.ts
+++ packages/did-comm/src/didcomm.ts
@@ -129,7 +129,7 @@ export interface ISendMessageDIDCommAlpha1Args {
  *
  * @beta This API may change without a BREAKING CHANGE notice.
  */
-export interface DIDCOmmConfig<T = IDIDCommTransport> {
+export interface DIDCOmmConfig<T extends IDIDCommTransport = DIDCommHttpTransport> {
   transports?: T[]
   isMediateDefaultGrantAll?: boolean
 }
