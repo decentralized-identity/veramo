2ea1c858a66396792f212c3b2b9aa366e7f4c3ae
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-11-02 11:10:06 +0700

    feat: add MedationManager methods

--- packages/mediation-manager/src/mediation-manager.ts
+++ packages/mediation-manager/src/mediation-manager.ts
@@ -1,5 +1,51 @@
 import type { IAgentPlugin } from '@veramo/core-types'
 
 export class MediationManager implements IAgentPlugin {
-  // logic
+  readonly methods: any
+
+  constructor() {
+    this.methods = {
+      mediationManagerSaveMediationPolicy: this.saveMediationPolicy.bind(this),
+    }
+  }
+
+  public async saveMediationPolicy(args: any, context: any) {
+    // logic
+  }
+
+  public async removeMediationPolicy(args: any, context: any) {
+    // logic
+  }
+
+  public async isMediationGranted(args: any, context: any) {
+    // logic
+  }
+
+  public async getMediationPolicies(args: any, context: any) {
+    // logic
+  }
+
+  public async saveMediation(args: any, context: any) {
+    // logic
+  }
+
+  public async getMediation(args: any, context: any) {
+    // logic
+  }
+
+  public async removeMediation(args: any, context: any) {
+    // logic
+  }
+
+  public async addRecipientDid(args: any, context: any) {
+    // logic
+  }
+
+  public async removeRecipientDid(args: any, context: any) {
+    // logic
+  }
+
+  public async getRecipientDids(args: any, context: any) {
+    // logic
+  }
 }
