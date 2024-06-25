3b0872e894b085a17a9b793ae0885ece0b3c9fc7
nickreynolds <nicholas.s.reynolds@gmail.com>
2024-06-24 21:55:16 -0400

    chore(credential-w3c): cleanup

--- __tests__/localMemoryStoreAgent.test.ts
+++ __tests__/localMemoryStoreAgent.test.ts
@@ -110,7 +110,7 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
 
   const eip712 = new CredentialIssuerEIP712()
   const jwt = new CredentialIssuerJWT()
-  const lds = new CredentialIssuerLD({
+  const ld = new CredentialIssuerLD({
     contextMaps: [LdDefaultContexts, credential_contexts as any],
     suites: [
       new VeramoEcdsaSecp256k1RecoverySignature2020(),
@@ -212,7 +212,7 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
       new CredentialPlugin([eip712, jwt, lds]),
       eip712,
       jwt,
-      lds,
+      ld,
       new SelectiveDisclosure(),
       ...(options?.plugins || []),
     ],
