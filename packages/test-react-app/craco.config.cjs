dc5e9eca82fcd4ebd17aa615206d042d8c954e33
veromassera <veromassera@yahoo.com>
2024-08-21 10:37:23 -0300

    vm setting

--- packages/test-react-app/craco.config.cjs
+++ packages/test-react-app/craco.config.cjs
@@ -1,5 +1,10 @@
 module.exports = {
     babel: {
         plugins: ["@babel/plugin-syntax-import-assertions"]
-    }
+    },
+    resolve: {
+        fallback: {
+          "vm": false
+        }
+      }
 }
