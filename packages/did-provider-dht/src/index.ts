61b5a677711434ee45d3c9c0493a7e393eabd655
walnut07 <p20080mk@gm.tsuda.ac.jp>
2024-08-27 19:11:43 +0900

    chore: init package

---
+++ packages/did-provider-dht/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Provides `did:dht` {@link @veramo/did-provider-dht#DHTDIDProvider | identifier provider } for the
+ * {@link @veramo/did-manager#DIDManager}
+ *
+ * @packageDocumentation
+ */
+export { DHTDIDProvider } from './dht-did-provider.js'
