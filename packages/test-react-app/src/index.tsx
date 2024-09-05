26d96ecaf6055e54851c7adbc2a608277f2cb14b
nickreynolds <nicholas.s.reynolds@gmail.com>
2024-09-05 11:47:13 -0400

    chore(test-react-app): refactor deprecated call to ReactDOM.render

--- packages/test-react-app/src/index.tsx
+++ packages/test-react-app/src/index.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import './global'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!); 
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
