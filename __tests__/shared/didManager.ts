8900f0beb642010c3abdecb828a5c13df02d7afb
arwykchorres <arwykchorres@gmail.com>
2025-04-29 14:49:33 -0500

    feat(did-manager): add 'isLocal' in 'options' to only modify DIDStore

--- __tests__/shared/didManager.ts
+++ __tests__/shared/didManager.ts
@@ -1,6 +1,6 @@
 // noinspection ES6PreferShortImport
 
-import { IDIDManager, IIdentifier, IKeyManager, TAgent } from '../../packages/core-types/src'
+import { IDIDManager, IIdentifier, IKeyManager, ManagedKeyInfo, TAgent } from '../../packages/core-types/src'
 
 type ConfiguredAgent = TAgent<IDIDManager & IKeyManager>
 
@@ -465,5 +465,112 @@ export default (testContext: {
         /illegal_argument: Identifier with alias:.*already exists.*but was created with a different provider.*/,
       )
     })
+
+    it('should add key only in DIDStore', async () => {
+      const webIdentifier = await agent.didManagerGetOrCreate({
+        alias: 'did.example.com',
+        provider: 'did:web',
+      })
+
+      const mockNewKey: ManagedKeyInfo = {
+        "type": "X25519",
+        "kid": "ec38b35a3cdf99d2aac3beecebe70e1451869a69d02cba923d0eed1f3969065e",
+        "publicKeyHex": "ec38b35a3cdf99d2aac3beecebe70e1451869a69d02cba923d0eed1f3969065e",
+        "meta": {
+          "algorithms": [
+            "ECDH",
+            "ECDH-ES",
+            "ECDH-1PU"
+          ]
+        },
+        "kms": "local"
+      }
+
+      const result = await agent.didManagerAddKey({
+        did: webIdentifier.did,
+        key: mockNewKey,
+        options: { isLocal: true },
+      })
+
+      expect(result).toEqual(true)
+
+      const updatedIdentifier = await agent.didManagerGet({ did: webIdentifier.did })
+      expect(updatedIdentifier.keys.some((key: any) => key.kid === mockNewKey.kid)).toBe(true)
+    })
+
+    it('should remove key from DIDStore', async () => {
+      const webIdentifier = await agent.didManagerGet({
+        did: 'did:web:did.example.com',
+      })
+
+      const mockNewKey: ManagedKeyInfo = {
+        "type": "X25519",
+        "kid": "ec38b35a3cdf99d2aac3beecebe70e1451869a69d02cba923d0eed1f3969065e",
+        "publicKeyHex": "ec38b35a3cdf99d2aac3beecebe70e1451869a69d02cba923d0eed1f3969065e",
+        "meta": {
+          "algorithms": [
+            "ECDH",
+            "ECDH-ES",
+            "ECDH-1PU"
+          ]
+        },
+        "kms": "local"
+      }
+
+      const result = await agent.didManagerRemoveKey({
+        did: webIdentifier.did,
+        kid:  mockNewKey.kid,
+        options: { isLocal: true },
+      })
+
+      expect(result).toEqual(true)
+
+      const updatedIdentifier = await agent.didManagerGet({ did: webIdentifier.did })
+      expect(updatedIdentifier.keys.some((key: any) => key.kid === mockNewKey.kid)).toBe(false)
+    })
+
+    it('should add service only in DIDStore', async () => {
+      const webIdentifier = await agent.didManagerGet({
+        did: 'did:web:did.example.com',
+      })
+
+      const mockService = {
+        id: 'did.example.com#didcomm_service',
+        type: 'Messaging',
+        serviceEndpoint: 'https://did.example.com/messaging',
+        description: 'Handles incoming messages',
+      }
+
+      const result = await agent.didManagerAddService({
+        did: webIdentifier.did,
+        service: mockService,
+        options: { isLocal: true },
+      });
+
+      expect(result).toEqual(true);
+
+      const updatedIdentifier = await agent.didManagerGet({ did: webIdentifier.did });
+      expect(updatedIdentifier.services.some((service: any) => service.id === mockService.id)).toBe(true)
+    });
+
+    it('should remove service from identifier with isLocal=true', async () => {
+      const webIdentifier = await agent.didManagerGet({
+        did: 'did:web:did.example.com',
+      });
+      const mockService = {
+        id: 'did.example.com#didcomm_service',
+      }
+
+      const result = await agent.didManagerRemoveService({
+        did: webIdentifier.did,
+        id: mockService.id,
+        options: { isLocal: true },
+      });
+
+      expect(result).toEqual(true);
+
+      const updatedIdentifier = await agent.didManagerGet({ did: webIdentifier.did });
+      expect(updatedIdentifier.services.some((service: any) => service.id === mockService.id)).toBe(false)
+    });
   })
 }
